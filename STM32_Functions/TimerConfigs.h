74593c85f28d6381f1fcd289dbde8e155ebddd7c
Michelle Bausager <46285548+Bausager@users.noreply.github.com>
2022-04-27 07:39:18 +0200

    Add files via upload

---
+++ STM32_Functions/TimerConfigs.h
@@ -0,0 +1,18 @@
+/*
+ * TimerConfigs.h
+ *
+ *  Created on: Dec 5, 2020
+ *      Author: Bausa
+ */
+
+#ifndef INC_TIMERCONFIGS_H_
+#define INC_TIMERCONFIGS_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+uint8_t TIM_freq(uint8_t htim, float Hz);
+
+
+#endif /* INC_TIMERCONFIGS_H_ */
